500a98c3e05bf064952c532aeb904173e2d4c761
Bryan-Cee <cheruiyotbryan@gmail.com>
2020-01-26 13:52:21 +0300

    add create scream mutation

--- functions/index.js
+++ functions/index.js
@@ -11,8 +11,26 @@ exports.getScreams = functions.https.onRequest((request, response) => {
   admin.firestore().collection('screams').get()
     .then(data => {
       let screams = [];
-      data.forEach(doc => screams.push(doc.data()))
+      data.forEach(doc => screams.push(doc.data()));
       return response.json(screams);
     })
     .catch(err => console.error(err))
-})
+});
+
+exports.createScreams = functions.https.onRequest(((req, res) => {
+  if(req.method !== 'POST'){
+    return res.status(400).json({ error: "Method not allowed"})
+  }
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin.firestore().collection('scream').add(newScream)
+    .then(doc => res.json({ message: `document ${doc.id} created successfuly`}))
+    .catch(err => {
+      res.status(500).json({ error: "Something went wrong"});
+      console.error(err);
+    })
+}));
