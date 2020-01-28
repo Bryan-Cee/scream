2a6aa2e2e2b27abde5f37b5f4fc9fb9de45dbf66
Bryan-Cee <cheruiyotbryan@gmail.com>
2020-01-28 16:32:44 +0300

    fix config

--- functions/utils/config.js
+++ functions/utils/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  apiKey: "AIzaSyBUNN0gBPeREChsTmndyY51GA8r3K3yI_Y",
-  authDomain: "scream-fe4f4.firebaseapp.com",
-  databaseURL: "https://scream-fe4f4.firebaseio.com",
-  projectId: "scream-fe4f4",
-  storageBucket: "scream-fe4f4.appspot.com",
-  messagingSenderId: "1082212805476",
-  appId: "1:1082212805476:web:f237414aeba52c06326871",
-  measurementId: "G-FTWCTG4EY2"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_PROJECT_ID,
+  measurementId: process.env.REACT_APP_APP_ID
 };
